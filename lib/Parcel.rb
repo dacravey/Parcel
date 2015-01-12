3262d21bce6334783dc78ecfe6210010e128631c
Debbie Cravey & Karvari Ellingson <student@epicodus.com>
2015-01-12 14:44:28 -0800

    add functionality for calculating postage based on volume and weight.

--- lib/Parcel.rb
+++ lib/Parcel.rb
@@ -1,8 +1,16 @@
 class Parcel
   define_method(:initialize) do
+    @volume
   end
 
   define_method(:volume) do |length, width, height|
-    volume = length.*(width.*(height))
+    @volume = length.*(width.*(height))
+  end
+
+  define_method(:cost_to_ship) do |weight|
+    volume_range = { (12..125) => 1, (126..1000) => 2, (1001..5000) => 3, (5001..25000) => 4,(25001..1000000) => 5 }
+    tier = volume_range.select {|tier| tier === @volume }.values.first
+    formula = tier.*(weight)
+    formula
   end
 end
--- spec/Parcel_spec.rb
+++ spec/Parcel_spec.rb
@@ -8,4 +8,12 @@ describe(Parcel) do
       expect(test_parcel.volume(10, 15, 20)).to eq(3000)
     end
   end
+
+  describe("#cost_to_ship") do
+    it("returns a cost based on pricing formula") do
+      test_parcel = Parcel.new()
+      test_parcel.volume(10, 15, 20)
+      expect(test_parcel.cost_to_ship(10)).to eq(30)
+    end
+  end
 end
